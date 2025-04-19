a89c9e191dcb2ad7f713b8be48254af4a5b7bba5
Pratham <pratham.sharma2105@gmail.com>
2025-04-19 22:42:10 +0530

    Refactor BloomFilter examples for clarity and consistency

--- main.go
+++ main.go
@@ -9,13 +9,22 @@ import (
 
 
 func main() {
-	// BloomFilter example with bool
+	/*
+	* Example of BloomFilter by using bool type
+	* using one hash function
+	*/
 	// f := bloombool.NewBloomFilter(100)
 	// f.Set("hello")
 	// f.Set("world")
 	// fmt.Println("is pratham exist", f.Check("pratham"))
 	// fmt.Println("is hello exist", f.Check("hello"))
 
+	/*
+	* Example of BloomFilter by using byte for saving space
+	* using multiple hash functions
+	*/
+	
+	// enter bloom filter size and number of hash functions
 	f := bloombytes.NewBloom(1000, 3)
 	f.Set("pratham")
 	f.Set("hello")
@@ -24,11 +33,9 @@ func main() {
 
 
 	fmt.Println("is pratham", f.Check("pratham"))
-	fmt.Println("is mahesh", f.Check("mahesh"))
-	fmt.Println("is mohan", f.Check("mohan"))
 	fmt.Println("is hi", f.Check("hi"))
+	fmt.Println("is testtttt", f.Check("testtttt"))
 	fmt.Println("is hello", f.Check("hello"))
 	fmt.Println("is test", f.Check("test"))
 	fmt.Println("is hiii", f.Check("hiii"))
-
 }
